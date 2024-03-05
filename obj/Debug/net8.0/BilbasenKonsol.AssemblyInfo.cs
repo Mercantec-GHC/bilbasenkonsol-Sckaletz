fc7d6f0389493d3d613ea07a6da9a2d352735e66
Sckaletz <allan@leeloo.dk>
2024-03-05 14:10:14 +0100

    Abstract dag done

--- obj/Debug/net8.0/BilbasenKonsol.AssemblyInfo.cs
+++ obj/Debug/net8.0/BilbasenKonsol.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BilbasenKonsol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32244d19323d6519fbdd588299f2b37e2c4cd2d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802b09c4a1162d862e9a8aa96e44a1108e9467c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BilbasenKonsol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BilbasenKonsol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
